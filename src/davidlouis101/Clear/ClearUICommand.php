002d9ee4b5d0b1b5923329864afbe338914e0d42
CrowBaldeYT <54008552+davidlouis101@users.noreply.github.com>
2020-10-09 20:12:42 +0200

    Update ClearUICommand.php

--- src/davidlouis101/Clear/ClearUICommand.php
+++ src/davidlouis101/Clear/ClearUICommand.php
@@ -16,8 +16,25 @@ use pocketmine\utils\TextFormat as TF;
 class ClearUICommand PluginBase implements Listener {
 
  public function onEnable(){
- $this->getLogger()->info(TF::GREEN . "Plugin Startet");
+ $this->getLogger()->info(TF::GREEN . "Plugin Start");
  }
- 
- 
+
+ public function onCommand(CommandSender $s, Command $c, string $label, array $args): bool
+  {
+   
+    switch($c->getName()){
+      if($s instanceof Player){
+       $this->Clear($s);
+     } else {
+       $s->sendMessage(TextFormat::DADK_RED . "Bitte Benutze denn Command In-Game!");
+     }
+    }
+  break;
+   }
+   return true;
+}
+
+ public Clear($player){
+
+ }
 }
